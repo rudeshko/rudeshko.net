74df413545694c0f6fcc407a1b35c736179dca1e
Dima Rudeshko <dima@rudeshko.net>
2019-07-01 16:48:29 -0400

    Init

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { Link } from "gatsby"
+
+const Footer = () => (
+  <footer>
+    
+  </footer>
+)
+
+export default Footer
---
+++ src/components/terminal.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Terminal = () => (
+  <div>
+    
+  </div>
+)
+
+export default Terminal
